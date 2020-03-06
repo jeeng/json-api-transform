8a67c1638e8b5dad2e8643b57461984b257d2920
Roma Bronstein <outsidenote@gmail.com>
2020-03-07 01:04:31 +0200

    Added tests

---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+describe(`@JAT tests`, function() {
+  it("@test1 - mapping of scalars (string, boolean and number)", () => {
+    throw new Error("NOP");
+  });
+  it("@test2 - mapping of an array", () => {
+    throw new Error("NOP");
+  });
+  it("@test3 - mapping of an object", () => {
+    throw new Error("NOP");
+  });
+  it("@test4 - mapping with array_trasform", () => {
+    throw new Error("NOP");
+  });
+});
